40d349b42eff880e0e6b59dae3daaa5a6d76ed5d
Fernie2 <ferniemendez@live.com>
2025-03-05 21:26:28 -0700

    Styalized the settings page to fit color scheme

--- src/components/pages/SettingsSidebar.tsx
+++ src/components/pages/SettingsSidebar.tsx
@@ -1,38 +1,42 @@
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, 
-         SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator,} from "~/components/ui/sidebar";
-import {useSettingsContext } from "../utils/SettingsContext";
+import {
+    Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel,
+    SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator,
+} from "~/components/ui/sidebar";
+import { useSettingsContext } from "../utils/SettingsContext";
 
-export function SettingsSidebar(){
+export function SettingsSidebar() {
     const { menuItem, setMenuItem } = useSettingsContext();
 
     return (
         <Sidebar>
-            <SidebarHeader className="text-xl">
+            <SidebarHeader className="text-2xl bg-gray">
                 Settings Options
             </SidebarHeader>
             <SidebarSeparator />
-            <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupLabel className="text-base">
+            <SidebarContent >
+                <SidebarGroup className="text-xl bg-gray">
+                    <SidebarGroupLabel className="text-base text-xl">
                         Manage Account
                     </SidebarGroupLabel>
-                     <SidebarGroupContent>
+                    <SidebarGroupContent>
                         <SidebarMenu>
                             <SidebarMenuItem>
-                                <SidebarMenuButton onClick={() => setMenuItem("Password")} className = "text-base">
+                                <SidebarMenuButton onClick={() => setMenuItem("Password")}
+                                    className={`text-base px-2 py-1 hover:bg-[#b0e0e6]/50 rounded-md transition-colors ${menuItem === "Password" ? "bg-[#b0e0e6]" : "bg-transparent"}`}>
                                     Change Password
-                                 </SidebarMenuButton>
+                                </SidebarMenuButton>
                             </SidebarMenuItem>
                             <SidebarMenuItem>
-                                <SidebarMenuButton onClick={() => setMenuItem("Email")} className="text-base">
+                                <SidebarMenuButton onClick={() => setMenuItem("Email")}
+                                    className={`text-base px-2 py-1 hover:bg-[#b0e0e6]/50 rounded-md transition-colors  ${menuItem === "Email" ? "bg-[#b0e0e6]" : "bg-transparent"}`}>
                                     Change Email
                                 </SidebarMenuButton>
                             </SidebarMenuItem>
                         </SidebarMenu>
                     </SidebarGroupContent>
                 </SidebarGroup>
-                <SidebarSeparator/>
+                <SidebarSeparator />
             </SidebarContent>
-        </Sidebar>
+        </Sidebar >
     )
 }
